9329a1fa36d8677a7461afab4a9be5a74774312c
xindic <ayaanlums@gmail.com>
2024-10-14 15:17:31 +0500

    Initialized

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,8 @@
 	import ThemeToggle from "../components/theme-toggle.svelte"
   import Navbar from "../components/navbar.svelte";
 
+	export const prerender = true;
+
 	export function load() {
   	theme.subscribe(value => {
     	document.documentElement.className = value;
