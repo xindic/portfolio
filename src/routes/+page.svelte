048f7ef9a71352ad1f98ae6017e85e5867f02142
xindic <ayaanlums@gmail.com>
2024-10-14 20:26:08 +0500

    Initialized

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script>
-    import Home from "../components/home.svelte";
-		import About from "../components/about.svelte";
-    import Projects from "../components/projects.svelte";
-    import SectionDivider from "../components/section-divider.svelte";
-    import Skills from "../components/skills.svelte";
-    import Contact from "../components/contact.svelte";
-    import Footer from "../components/footer.svelte";
+	import Home from "../components/home.svelte";
+	import About from "../components/about.svelte";
+	import Projects from "../components/projects.svelte";
+	import SectionDivider from "../components/section-divider.svelte";
+	import Skills from "../components/skills.svelte";
+	import Contact from "../components/contact.svelte";
+	import Footer from "../components/footer.svelte";
 </script>
 
 <Home />
